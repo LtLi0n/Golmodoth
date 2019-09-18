ecf6004f6067f7fc5ba60d99c38bccf1db21a110
LtLi0n <LtLi0n@users.noreply.github.com>
2019-09-18 23:45:23 +0300

    Size struct because size matters.

---
+++ Golmodoth.Client/Framework/Core/Size.cs
@@ -0,0 +1,20 @@
+﻿namespace Golmodoth.Client
+{
+    public struct Size : ISizeable
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public Size(ISizeable size)
+        {
+            Width = size.Width;
+            Height = size.Height;
+        }
+    }
+}
