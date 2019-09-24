b201df50c482d852c6546832af9e1f5121731bbf
LtLi0n <saltenis46@gmail.com>
2019-09-25 00:15:07 +0300

    Experimenting with permission model.

---
+++ Golmodoth.Shared/Authentification/ApiKey.cs
@@ -0,0 +1,34 @@
+﻿using Golmodoth.Shared;
+using LionLibrary.SQL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Golmodoth.Shared
+{
+    public class ApiKey : IEntity<ApiKey, int>
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        [Column(TypeName = "char(32)"), Required]
+        public string Key { get; set; }
+
+        public bool IsValid { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public IReadOnlyCollection<string> Roles { get; }
+
+        public static void CreateModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApiKey>(x =>
+            {
+                x.HasIndex(apiKey => apiKey.Key).IsUnique();
+            });
+        }
+    }
+}
---
+++ Golmodoth.Shared/Authentification/Permission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Golmodoth.Shared
+{
+    [Flags]
+    public enum Permission : byte
+    {
+        None = 0b_0000_0000,
+        All = 0b_0000_1111,
+        
+        Create = 0b_0000_0001,
+        Read = 0b_0000_0010,
+        Update = 0b_0000_0100,
+        Remove = 0b_0000_1000,
+    }
+}
