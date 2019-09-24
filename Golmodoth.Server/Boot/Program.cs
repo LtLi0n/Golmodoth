b8e5d493a7d94a2e6b966fa07f8747859264d911
LtLi0n <saltenis46@gmail.com>
2019-09-25 00:20:23 +0300

    Adding main server initial files. Very empty, such desert.

---
+++ Golmodoth.Server/Boot/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Golmodoth.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args) => await new Startup(args).StartAsync();
+    }
+}
---
+++ Golmodoth.Server/Boot/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golmodoth.Server
+{
+    public class Startup
+    {
+        public IReadOnlyCollection<string> Args { get; }
+
+        public Startup(string[] args)
+        {
+            Args = new ReadOnlyCollection<string>(args);
+        }
+
+        public async Task StartAsync()
+        {
+            await Task.Delay(-1);
+        }
+    }
+}
